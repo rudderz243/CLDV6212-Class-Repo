d7476ec84deb76a674398c3e3dcaa5542ede781b
Glynn <79942619+rudderz243@users.noreply.github.com>
2026-07-22 14:20:29 +0200

    g2 finish functions

--- FirstFunctionG2/FirstFunctionG2/Controllers/HomeController.cs
+++ FirstFunctionG2/FirstFunctionG2/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace FirstFunctionG2.Controllers
 		{
 			return View();
 		}
-
+		[HttpPost]
 		public async Task<IActionResult> Index(string userName) {
 			// check whether username is present or not
 			if (string.IsNullOrEmpty(userName))
@@ -20,12 +20,40 @@ namespace FirstFunctionG2.Controllers
 			}
 
 			// set up the connection to our function
-			var functionUrl = "http://localhost:7071/api/ProcessDataFunction";
+			var functionUrl = "http://localhost:7240/api/ProcessDataFunction";
 
 			// in order to pass data through to a function, we need to either use the BODY or the PARAMETERS of the request
 			// when specifying a parameter, you specify the name using a ? to indicate the beginning, and an = for the end
 			// ?name= means that "name" is the parameter name, anything after the = is the value.
 			var apiRequestUrl = $"{functionUrl}?name={userName}";
+
+			try {
+				// the next few lines in between the try { brackets } will attempt to run, and if anything bad happens
+				// the catch { brackets } will run instead of the code completing
+
+				using (var client = new HttpClient()) {
+					// when we talk to a server/function/api, we do so by making a REQUEST
+					// what we get back, is called a RESPONSE
+					var response = await client.GetAsync(apiRequestUrl);
+					
+					// after we receieve a response, we need to first check whether the server was successful\
+					// in running our code, or not
+					if (response.IsSuccessStatusCode) {
+						// if the function ran successfully, get the content it sent back to us, as a string
+						var result = await response.Content.ReadAsStringAsync();
+						// pass through the content to the frontend
+						ViewBag.FuncResponse = result;
+					} else {
+						// if it was NOT successful, then:
+						ViewBag.FuncResponse = $"Error Code: {response.StatusCode}";
+					}
+				}
+			} catch (HttpRequestException ex) {
+				// if an exception occurs attempting to make an HTTP request, print out the error message
+				ViewBag.FuncResponse = $"Error: {ex.Message}";
+			}
+			// show the page now that we've fetched the content from the function
+			return View();
 		}
 
 		public IActionResult Privacy()
