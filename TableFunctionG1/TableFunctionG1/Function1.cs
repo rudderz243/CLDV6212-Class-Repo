5bac83cd9dd2518d92dc5fb697dc592561ded63d
Glynn <79942619+rudderz243@users.noreply.github.com>
2026-08-06 15:38:01 +0200

    g1 + 2 table crud

--- TableFunctionG1/TableFunctionG1/Function1.cs
+++ TableFunctionG1/TableFunctionG1/Function1.cs
@@ -1,3 +1,4 @@
+using Azure;
 using Azure.Data.Tables;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 using System.Text.Json;
 using TableFunctionG1.Models;
 
@@ -13,7 +15,7 @@ namespace TableFunctionG1;
 public class BookTableFunctions
 {
 	// TableClient -> our connection to the TableService using our Singleton from earlier
-	private readonly TableClient _tableClient; 
+	private readonly TableClient _tableClient;
 
 	// we modify the constructor to create the connection when the function starts up
 	public BookTableFunctions(TableServiceClient tableServicClient)
@@ -24,20 +26,22 @@ public class BookTableFunctions
 	}
 
 	[Function("CreateNewBook")]
-	public async Task<HttpResponseData> CreateNewBook([HttpTrigger(AuthorizationLevel.Anonymous, "post", 
+	public async Task<HttpResponseData> CreateNewBook([HttpTrigger(AuthorizationLevel.Anonymous, "post",
 	Route = "books")] HttpRequestData req)
 	{
 		// get the book data from the request
 		var book = await JsonSerializer.DeserializeAsync<BookEntity>(req.Body);
 
-		if (book == null) {
+		if (book == null)
+		{
 			var badResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest); // (code 400)
 			await badResponse.WriteStringAsync("Invalid book information provided.");
 			return badResponse;
 		}
 
 		// if required data is missing: return 400
-		if (string.IsNullOrWhiteSpace(book.Title) || string.IsNullOrEmpty(book.Author)) {
+		if (string.IsNullOrWhiteSpace(book.Title) || string.IsNullOrEmpty(book.Author))
+		{
 			var badResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest); // (code 400)
 			await badResponse.WriteStringAsync("Invalid book information provided.");
 			return badResponse;
@@ -52,7 +56,8 @@ public class BookTableFunctions
 
 	[Function("GetBooksByGenre")]
 	public async Task<HttpResponseData> GetBooksByGenre([HttpTrigger(AuthorizationLevel.Anonymous, "get",
-	Route="books/{genre}")] HttpRequestData req, string genre) {
+	Route="books/{genre}")] HttpRequestData req, string genre)
+	{
 		// get books from table
 		var books = new List<BookEntity>();
 
@@ -68,5 +73,89 @@ public class BookTableFunctions
 		var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
 		await response.WriteAsJsonAsync(books);
 		return response;
+	}
+
+	[Function("GetBookById")]
+	public async Task<HttpResponseData> GetBookById([HttpTrigger(AuthorizationLevel.Anonymous, "get",
+	Route = "books/{genre}/{id}")] HttpRequestData req, string genre, string id)
+	{
+		try
+		{
+			// to get an item from TableStorage, you need to provide both the paritionKey and the rowKey
+			var requestedEntity = await _tableClient.GetEntityAsync<BookEntity>(genre, id);
+			// if we are able to find it (no errors), we return it
+			var goodResponse = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			await goodResponse.WriteAsJsonAsync(requestedEntity);
+			return goodResponse;
+		}
+		catch (RequestFailedException ex) when (ex.Status == 404)
+		{
+			// when things aren't found, we return a 404 to the user/client/requesting service
+			var badResponse = req.CreateResponse(HttpStatusCode.NotFound);
+			await badResponse.WriteStringAsync($"error: item not found with id {id}");
+			return badResponse;
+		}
+		catch (Exception ex)
+		{
+			// return 500
+			var genericResponse = req.CreateResponse(HttpStatusCode.InternalServerError);
+			await genericResponse.WriteStringAsync("error: server explod");
+			return genericResponse;
+		}
+	}
+
+	[Function("UpdateBook")]
+	public async Task<HttpResponseData> UpdateBook([HttpTrigger(AuthorizationLevel.Anonymous, "put",
+	Route = "books/{genre}/{id}")] HttpRequestData req, string genre, string id)
+	{
+		// get update book information from the req body
+		var updatedBook = await JsonSerializer.DeserializeAsync<BookEntity>(req.Body);
+
+		if (updatedBook == null)
+		{
+			var blankResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+			await blankResponse.WriteStringAsync("error: invalid data provided");
+			return blankResponse;
+		}
+		// set the partition and the row keys based on the users request
+		updatedBook.PartitionKey = genre;
+		updatedBook.RowKey = id;
+
+		try
+		{
+			// using the new info, update EVERYTHING, and replace the existing entity
+			await _tableClient.UpdateEntityAsync(updatedBook, ETag.All, TableUpdateMode.Replace);
+			var goodResponse = req.CreateResponse(HttpStatusCode.OK);
+			await goodResponse.WriteStringAsync("message: book successfully updated");
+			return goodResponse;
+		}
+		catch (RequestFailedException ex) when (ex.Status == 404)
+		{
+			// when things aren't found, we return a 404 to the user/client/requesting service
+			var badResponse = req.CreateResponse(HttpStatusCode.NotFound);
+			await badResponse.WriteStringAsync($"error: item not found with id {id}");
+			return badResponse;
+
+		}
+	}
+
+	[Function("DeleteBook")]
+	public async Task<HttpResponseData> DeleteBook([HttpTrigger(AuthorizationLevel.Anonymous, "delete",
+	Route = "books/{genre}/{id}")] HttpRequestData req, string genre, string id) {
+		try {
+			await _tableClient.DeleteEntityAsync(genre, id);
+			var response = req.CreateResponse(HttpStatusCode.NoContent);
+			return response;
+		}
+		catch (RequestFailedException ex) when (ex.Status == 404)
+		{
+			// when things aren't found, we return a 404 to the user/client/requesting service
+			var badResponse = req.CreateResponse(HttpStatusCode.NotFound);
+			await badResponse.WriteStringAsync($"error: item not found with id {id}");
+			return badResponse;
+
+		}
+
+
 	}
 }
--- TableFunctionG2/TableFunctionG2/Function1.cs
+++ TableFunctionG2/TableFunctionG2/Function1.cs
@@ -1,23 +1,85 @@
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using TableFunctionG2.Models;
 
 namespace TableFunctionG2;
 
-public class Function1
+public class BookTableFunction
 {
-    private readonly ILogger<Function1> _logger;
+	// call in the table client - so that our app can communicate with the azure table (using our singleton)
+	private readonly TableClient _tableClient;
+	
+	public BookTableFunction(TableServiceClient tableService)
+	{
+		// assign the table to the local variable, using our singleton
+		_tableClient = tableService.GetTableClient("books");
+		// create the table if it does not already exist
+		_tableClient.CreateIfNotExists();
+	}
 
-    public Function1(ILogger<Function1> logger)
-    {
-        _logger = logger;
-    }
+	[Function("AddBook")]
+	public async Task<HttpResponseData> AddBook([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "books")]
+	HttpRequestData req) {
+		// we get the request data from the body, and turn it from JSON into a new book entity
+		var newBook = await JsonSerializer.DeserializeAsync<BookEntity>(req.Body);
 
-    [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-    {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
-    }
+		// check whether we were able to successfully decode the information
+		if (newBook == null) {
+			var badResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+			await badResponse.WriteStringAsync("error: invalid information passed");
+			return badResponse;
+		}
+
+		// check if all required information is present
+		if (string.IsNullOrWhiteSpace(newBook.Title) || string.IsNullOrWhiteSpace(newBook.PartitionKey) ||
+		string.IsNullOrWhiteSpace(newBook.RowKey)) {
+			var badResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+			await badResponse.WriteStringAsync("error: missing required information");
+			return badResponse;
+		}
+
+		// if we pass the error checking phase, we add the book to the table
+		await _tableClient.AddEntityAsync(newBook);
+
+		// return a good response
+		var goodResponse = req.CreateResponse(System.Net.HttpStatusCode.Created);
+		await goodResponse.WriteStringAsync("message: book added succesfully");
+		return goodResponse;
+	}
+
+	[Function("GetBooksByGenre")]
+	public async Task<HttpResponseData> GetBooksByGenre([HttpTrigger(AuthorizationLevel.Anonymous, "get", 
+	Route = "books/{genre}")] HttpRequestData req, string genre) {
+		// start off with an empty list
+		var genreBooks = new List<BookEntity>();
+
+		// use LINQ to query the Azure Table, to find all books that match the specified Genre
+		var tableData = _tableClient.QueryAsync<BookEntity>(b => b.PartitionKey == genre);
+
+		// using a loop, add each of the query results to the list
+		await foreach (var item in tableData) {
+			genreBooks.Add(item);
+		}
+
+		// return a response to the user containing a list of all the books
+		var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+		await response.WriteAsJsonAsync(genreBooks);
+		return response;
+	}
+
+	[Function("GetBookById")]
+	public async Task<HttpResponseData> GetBookById ([HttpTrigger(AuthorizationLevel.Anonymous, "get",
+	Route = "books/{genre}/{id}")] HttpRequestData req, string genre, string id) {
+	
+	}
+
+
+
+
 }
