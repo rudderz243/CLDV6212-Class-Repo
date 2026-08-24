eb167daa6ca93580753f9a4bde893b3083575063
Glynn <79942619+rudderz243@users.noreply.github.com>
2026-08-24 16:34:43 +0200

    finish blob storage G1+2

---
+++ TableFunctionG1/TableFunctionG1/BlobFunctions.cs
@@ -0,0 +1,98 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TableFunctionG1
+{
+	public class BlobFunctions
+	{
+		// prepare to use our singleton from Program.cs
+		private readonly BlobContainerClient _blobClient;
+
+		// use a constructor to initialize our container client
+		public BlobFunctions(BlobServiceClient blobService) {
+			// open this specific "folder" in the blob service
+			_blobClient = blobService.GetBlobContainerClient("g1-blob");
+			// if it doesn't already exist, create it
+			_blobClient.CreateIfNotExists();
+		}
+
+		[Function("UploadBlob")]
+		public async Task<HttpResponseData> Upload([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route ="blob/upload/{filename}")]
+		HttpRequestData req, string filename) {
+			// try {} catch {}
+			// call in a request to the blob for this file
+			// check firstly whether the file already exists in the blob
+			BlobClient blob = _blobClient.GetBlobClient(filename);
+			// if a file already exists with this name, we overwrite it
+			await blob.UploadAsync(req.Body, overwrite: true);
+
+			// if no file -> 400
+
+			// if the file manages to upload successfully, 200 OK
+			var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			await response.WriteStringAsync($"file {filename} uploaded successfully.");
+			return response;
+
+
+			// catch -> response 500
+		}
+
+		[Function("DownloadBlob")]
+		public async Task<HttpResponseData> Download([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="blob/download/{filename}")]
+		HttpRequestData req, string filename) {
+			// check if filename provided -> if not 400
+			// try {} catch {}
+
+			// get the file
+			BlobClient blob = _blobClient.GetBlobClient(filename);
+
+			// if file doesn't exist -> 404
+			if (!await blob.ExistsAsync()) {
+				return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+			}
+
+			// we create a STREAM to send the data back to the client bit by bit
+			var stream = await blob.OpenReadAsync();
+
+			// first -> create response
+			var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			// second -> inform the user they are getting a FILE, not text/json
+			response.Headers.Add("Content-Type", "application/octet-stream");
+			// third -> stream the file
+			await stream.CopyToAsync(response.Body);
+			// finally -> once all the file has been sent, end the response
+			return response;
+		}
+
+		[Function("ListBlobs")]
+		public async Task<HttpResponseData> ListBlobs([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route="blobs/list")]
+		HttpRequestData req) {
+			// try {} catch {}
+
+			// create a blank list of type object to hold our information
+			// object -> is not a fixed or defined type
+			var fileList = new List<object>();
+
+			// fetch every file in the blob to iterate over them
+			await foreach (var item in _blobClient.GetBlobsAsync()) {
+				// get the information we require
+				fileList.Add(new
+				{
+					FileName = item.Name,
+					FileSize = item.Properties.ContentLength,
+					DateModified = item.Properties.LastModified
+				});
+			}
+
+			// return the information to the user
+			var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			await response.WriteAsJsonAsync(fileList);
+			return response;
+		}
+	}
+}
---
+++ TableFunctionG2/TableFunctionG2/BlobFunctions.cs
@@ -0,0 +1,85 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TableFunctionG2
+{
+	public class BlobFunctions
+	{
+		// prepare our class to make use of the singleton declared in Program.cs
+		private readonly BlobContainerClient _blobClient;
+
+		public BlobFunctions(BlobServiceClient blobService)
+		{
+		// wrap everything in try{} catch{}
+		// check for the correct file types
+		// return 400 for any user errors (wrong file, missing file, etc)
+		// return 500 for any server errors (cannot connect to azurite, upload failed, etc).
+
+			// open the specific container we are looking for
+			_blobClient = blobService.GetBlobContainerClient("example-blob");
+			// if it wasn't found, create it
+			_blobClient.CreateIfNotExists();
+		}
+
+		[Function("UploadBlob")]
+		public async Task<HttpResponseData> Upload([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route ="blobs/{filename}")]
+		HttpRequestData req, string filename)
+		{
+			// try and find if the file already exists
+			BlobClient blob = _blobClient.GetBlobClient(filename);
+			// if it exists: overwrite, otherwise, upload the file
+			await blob.UploadAsync(req.Body, overwrite: true);
+
+			var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			await response.WriteStringAsync("blob uploaded successfully");
+			return response;
+		}
+		[Function("DownloadBlob")]
+		public async Task<HttpResponseData> Download ([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="blobs/{filename}")]
+		HttpRequestData req, string filename) {
+			// try and get the file
+			BlobClient blob = _blobClient.GetBlobClient(filename);
+
+			// if file does NOT exist
+			if (!await blob.ExistsAsync()) {
+				// 404
+				return req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+			}
+			// otherwise write the file out
+			var stream = await blob.OpenReadAsync();
+			var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			// set the response TYPE using headers to indicate a file is coming instead of text/json
+			response.Headers.Add("Content-Type", "application/octet-stream");
+			await stream.CopyToAsync(response.Body);
+			return response;
+		}
+		[Function("ListBlob")]
+		public async Task<HttpResponseData> ListAll([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="blobs")]
+		HttpRequestData req) {
+			// create an empty list to store the information
+			// object -> because it contains files
+			var fileList = new List<object>();
+
+			// fetch every file in the blob storage
+			await foreach (var item in _blobClient.GetBlobsAsync()) {
+				// and add the relevant information to the list
+				fileList.Add(new
+				{
+					FileName = item.Name,
+					SizeBytes = item.Properties.ContentLength,
+					DateModified = item.Properties.LastModified
+				});
+			}
+			// once we have got the information for all the files, we return to the user
+			var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+			await response.WriteAsJsonAsync(fileList);
+			return response;
+		}
+	}
+}
